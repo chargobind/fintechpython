3b3a5f80796207927554187e62ae6633e728702f
chargobind <75035651+chargobind@users.noreply.github.com>
2021-02-08 14:52:09 -0500

    Harold Assign

---
+++ H_Calculator.py
@@ -0,0 +1,17 @@
+
+# original_price = "198.87"
+# current_price = "254.32"
+# increase = "55.45"
+# percent_increase = "27.8"
+
+num1 = input("Enter the original price: ")
+num2 =input("Enter the current price: ")
+print("This is your increase ")
+result = float(num2) - float(num1)
+print(result)
+
+print("Let's calculate your percentage!")
+num3 = input("Enter your Increase: ")
+num4 = input("Enter your Original Price: ")
+result2 = float(num3) / float(num1) * 100
+print(result2)
